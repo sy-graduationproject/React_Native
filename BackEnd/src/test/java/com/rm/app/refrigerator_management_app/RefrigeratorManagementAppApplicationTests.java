4318feae9a4931aa19e040cb0160d6849037eaa0
soosungp33 <soosungp33@gmail.com>
2021-04-02 16:36:48 +0900

    commit test

---
+++ BackEnd/src/test/java/com/rm/app/refrigerator_management_app/RefrigeratorManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rm.app.refrigerator_management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefrigeratorManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
