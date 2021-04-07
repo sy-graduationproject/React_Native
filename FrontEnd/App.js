66d601276ad9f523727ff0d36a38f216c13e5d67
한상현 <luck2901@naver.com>
2021-04-07 18:27:58 +0900

    Navigation

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,21 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Home from './Home';
+import Memo from './Memo';
+import Profile from './Profile';
 
-export default function App() {
+const Tab = createMaterialBottomTabNavigator();
+
+function MyTabs() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="white"
+      labelStyle={{ fontSize: 12 }}
+      barStyle={{ backgroundColor: 'tomato' }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Memo"
+        component={Memo}
+        options={{
+          tabBarLabel: 'Memo',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="lead-pencil" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class App extends React.Component{
+  render() {
+    return (
+      <NavigationContainer>
+        <MyTabs />
+      </NavigationContainer>
+    );    
+  }
+}
