0b7b9e8cb5450b39bcb447652bda74ede50e635a
한상현 <luck2901@naver.com>
2021-04-09 15:41:59 +0900

    Login

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,63 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import Home from './Home';
-import Memo from './Memo';
-import Profile from './Profile';
+import { createStackNavigator } from '@react-navigation/stack';
+//import 'react-native-gesture-handler';
+import Navigation from './Navigation'
+import Login from './Login';
+import Register from './Register';
 
-const Tab = createMaterialBottomTabNavigator();
+const Stack = createStackNavigator();
 
-function MyTabs() {
+export default function App() {
+
   return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      activeColor="white"
-      labelStyle={{ fontSize: 12 }}
-      barStyle={{ backgroundColor: 'tomato' }}
-    >
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="home" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Memo"
-        component={Memo}
-        options={{
-          tabBarLabel: 'Memo',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="lead-pencil" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={Profile}
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="account" color={color} size={26} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Navigation" component={Navigation} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
-
-export default class App extends React.Component{
-  render() {
-    return (
-      <NavigationContainer>
-        <MyTabs />
-      </NavigationContainer>
-    );    
-  }
 }
