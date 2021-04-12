a9bc12813ab0b1aa5ba8302c30b51bda8fa91e38
한상현 <luck2901@naver.com>
2021-04-12 14:23:06 +0900

    Update

--- FrontEnd/Memo.js
+++ FrontEnd/Memo.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Memo() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text> Memoemoemo</Text>
+        <View style={styles.container}>
+            <Text style={styles.header}> Memo</Text>
+            <View style = {styles.memo}>
+                <TouchableOpacity
+                activeOpacity={0.7}>
+                    <Ionicons name="ios-checkmark" size={30} color='#FFFFF'/>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 20,
+        marginBottom: 10,
+        flexDirection:'row',
+        justifyContent: 'center',
+    },
+    header: {
+        fontSize: 25,
+    },
+    memo: {
+        
+    }
+});
