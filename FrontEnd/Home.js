3f6dbf49d0cf43606bf95eae1a2ca77096371903
한상현 <luck2901@naver.com>
2021-05-31 18:45:06 +0900

    Test

--- FrontEnd/Home.js
+++ FrontEnd/Home.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Button} from 'react-native';
 
 
-export default function Home(){
+const Home = () =>{
+    const onSubmit = () => {
+        fetch("http://localhost:8080/api/memo")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                setMessage(myJson);
+                console.log(JSON.stringify(myJson));
+            })
+    }
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text> Hi</Text>
+            <Button
+                title="Submit"
+                onPress={() => onSubmit()}
+            />
         </View>
     );
 }
+
+export default Home;
