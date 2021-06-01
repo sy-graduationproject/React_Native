4d1ab3feed34c58883e46c76539173bcb848a196
한상현 <luck2901@naver.com>
2021-06-01 16:40:03 +0900

    List

--- FrontEnd/Home.js
+++ FrontEnd/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View , Button} from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
 
 
 const Home = () =>{
@@ -15,13 +15,101 @@ const Home = () =>{
     }
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text> Hi</Text>
+            <View style={styles.c}>
+                <View style={styles.r}>
+                    <TouchableOpacity
+                        style={styles.button}
+                    ><Text>Cucumber</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                    ><Text>Cucumber</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                    ><Text>Cucumber</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.r}>
+                    <TouchableOpacity
+                        style={styles.button}
+                    ><Text>Cucumber</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                    ><Text>Cucumber</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                    ><Text>Cucumber</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.r}>
+                    <TouchableOpacity
+                        style={styles.button}
+                    ><Text>Cucumber</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                    ><Text>Cucumber</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                    ><Text>Cucumber</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.r}>
+                    <TouchableOpacity
+                        style={styles.button}
+                    ><Text>Cucumber</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                    ><Text>Cucumber</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                    ><Text>Cucumber</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.r}>
+                    <TouchableOpacity
+                        style={styles.button}
+                    ><Text>Cucumber</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                    ><Text>Cucumber</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                    ><Text>Cucumber</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
             <Button
-                title="Submit"
+                title="Reload"
                 onPress={() => onSubmit()}
             />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    button: {
+        height: 100,
+        width: 100,
+        backgroundColor: 'skyblue',
+        color: 'white',
+        margin:20,
+        alignItems:'center'
+    },
+    r: {
+        flexDirection: 'row',
+    },
+    c: {
+        flexDirection: 'column',
+    }
+});
 
 export default Home;
